3b0219fe2c381e4ba696d5e2eba709a552673b5d
Nikita Petrenko <thenikitapetrenko@gmail.com>
2020-08-13 11:37:59 -0400

    Add drag and drop support

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from flask import Flask, redirect, request, jsonify, render_template
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_dropzone import Dropzone
 import os
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
 
-@app.route('/', methods=['GET'])
+# Dropzone settings
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+
+# Uploads settings
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        file_obj = request.files
+        for f in file_obj:
+            file = request.files.get(f)
+            print (file.filename)
+        return "uploading..."
     return render_template('index.html')
 
 if __name__ == '__main__':
